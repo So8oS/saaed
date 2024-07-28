e9953acc39c75fe47ad2e15ccef22c11ac2f8ff7
So8oS <ahmadothman033@gmail.com>
2024-07-28 20:46:43 +0400

    from recovered

--- src/app/components/bookForm.tsx
+++ src/app/components/bookForm.tsx
@@ -342,14 +342,16 @@ const Form = () => {
         (result) => {
           console.log(result.text);
           toast.success(
-            "Form submitted successfully 🎉\n Saeed team will contact you soon!"
+            t(
+              "Form submitted successfully 🎉\n Saeed team will contact you soon!"
+            )
           );
           reset();
           setIsSubmitting(false);
         },
         (error) => {
           console.log(error.text);
-          toast.error("Failed to submit the form, please try again later");
+          toast.error(t("Failed to submit the form, please try again later"));
           setIsSubmitting(false);
         }
       );
@@ -633,45 +635,131 @@ const Form = () => {
               )}
             </div>
             {selectedCity === "other" && (
-              <div className="flex flex-col mb-4">
-                <label className="font-semibold mb-2">{t("Other City")}</label>
-                <input
-                  {...register("otherCity", {
-                    required: selectedCity === "other",
-                  })}
-                  className="px-4 py-2 border rounded-lg"
-                  placeholder={t("Please specify your city")}
-                />
-                {errors.otherCity && (
-                  <span className="text-red-500 text-sm">
-                    {t("This field is required")}
-                  </span>
+              // select from gcc countries
+              <div className="pt-4">
+                <label>{t("Country:")}</label>
+                <select
+                  {...register("country", { required: "Country is required" })}
+                  className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3 cursor-pointer"
+                >
+                  <option value="" disabled selected>
+                    {t("Select Country")}
+                  </option>
+                  {gccCountries.map((country, index) => (
+                    <option key={index} value={country}>
+                      {t(country)}
+                    </option>
+                  ))}
+                </select>
+                {errors.country && (
+                  <p className="text-red-500 text-sm">
+                    {errors.country.message?.toString()}
+                  </p>
                 )}
               </div>
             )}
-            <div className="py-4">
-              <label>{t("Message:")}</label>
-              <textarea
-                rows={4}
-                {...register("message", {
-                  required: t("This field is required"),
-                })}
-                className="mt-2 w-full rounded-md bg-gray-100 p-1"
-              ></textarea>
-              {errors.message && (
+            <div className="pt-4">
+              <label>{t("Website:")}</label>
+              <input
+                type="text"
+                placeholder="https://example.com"
+                {...register("website", { required: t("Website is required") })}
+                className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3"
+              />
+              {errors.website && (
                 <p className="text-red-500 text-sm">
-                  {errors.message.message?.toString()}
+                  {errors.website.message?.toString()}
                 </p>
               )}
             </div>
+            <div className="pt-4">
+              <label>{t("Email Address:")}</label>
+              <input
+                type="email"
+                placeholder="Info@example.com"
+                {...register("email", {
+                  required: t("Email is required"),
+                  pattern: {
+                    value: /^\S+@\S+$/i,
+                    message: "Entered value does not match email format",
+                  },
+                })}
+                className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3"
+              />
+              {errors.email && (
+                <p className="text-red-500 text-sm">
+                  {errors.email.message?.toString()}
+                </p>
+              )}
+            </div>
+            <div className="grid gap-4 lg:grid-cols-2 pt-4">
+              <div>
+                <label>{t("Phone:")} </label>
+                <input
+                  type="text"
+                  placeholder="+543 5445 0543"
+                  {...register("phone", { required: t("Phone is required") })}
+                  className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3"
+                />
+                {errors.phone && (
+                  <p className="text-red-500 text-sm">
+                    {errors.phone.message?.toString()}
+                  </p>
+                )}
+              </div>
+            </div>
+            <div className="my-16">
+              <hr className="h-px bg-gray-200 border-0 dark:bg-gray-700" />
+            </div>
+            <div className="flex flex-col gap-8">
+              {questions.map((question, index) => (
+                <div key={index}>
+                  <label className="">{`${index + 1}- ${t(question)}`}</label>
+                  <textarea
+                    rows={4}
+                    {...register(`question${index + 1}`, {
+                      required: `Answer to question ${index + 1} is required`,
+                    })}
+                    className="mt-2 w-full rounded-md bg-gray-100 p-2 "
+                  ></textarea>
+                  {errors[`question${index + 1}`] && (
+                    <p className="text-red-500 text-sm">
+                      {t(errors[`question${index + 1}`]?.message?.toString())}
+                    </p>
+                  )}
+                </div>
+              ))}
+            </div>
           </>
         )}
-        <div className="text-center">
+        <div>
           <button
             type="submit"
-            className="w-full text-white bg-[#ed8972] rounded-lg p-2 my-4 md:w-1/2 md:m-auto"
+            className={`mt-5 w-full rounded-md bg-[#1f8598] p-2 text-center font-semibold text-white cursor-pointer  disabled:opacity-65 disabled:cursor-not-allowed `}
+            disabled={isSubmitting}
           >
-            {t("Submit")}
+            {isSubmitting ? (
+              <svg
+                aria-hidden="true"
+                className="inline w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+            ) : activeTab === "individual" ? (
+              t("Submit")
+            ) : (
+              t("Get Started")
+            )}
           </button>
         </div>
       </form>
